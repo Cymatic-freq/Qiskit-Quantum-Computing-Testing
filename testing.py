1590df38d00921647849e3241096dff2077dd51c
Cymatic-freq <60411342+Cymatic-freq@users.noreply.github.com>
2020-10-06 17:58:29 -0700

    Testing Basic Commands
    as title states

---
+++ testing.py
@@ -0,0 +1,6 @@
+print('Helloworld')
+#This is a comment you can create in python where nothing like this 
+#Here is a basic if statement in python
+if 10 > 5 :
+    print('Yes 10 is a bigger numer!')
+#lets explore lists and arrays
